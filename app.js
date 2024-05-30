7308b85ecd459926efee578c68ae2070ad9e7743
Miriam Foster <miriamfoster@Miriams-MacBook-Pro.local>
2024-05-30 12:29:11 -0700

    Commit

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 /** BizTime express application. */
-
-
 const express = require("express");
-
 const app = express();
 const ExpressError = require("./expressError")
 
 app.use(express.json());
 
+const companiesRoutes = require('./routes/companies');
+const invoicesRoutes = require('./routes/invoices')
+
+app.use('/companies', companiesRoutes);
+app.use('/invoices', invoicesRoutes);
 
 /** 404 handler */
-
 app.use(function(req, res, next) {
   const err = new ExpressError("Not Found", 404);
   return next(err);
 });
 
 /** general error handler */
-
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
 
@@ -26,6 +26,5 @@ app.use((err, req, res, next) => {
     message: err.message
   });
 });
-
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,2 +1,11 @@
 /** Database setup for BizTime. */
 
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: 'postgresql://localhost:5433/biztime'
+});
+
+client.connect();
+
+module.exports = client;
